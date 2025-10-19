3992783ac1de5a0efef473a3a20970c4c219b8e2
Aymina Çakır <114817398+ayminacakir@users.noreply.github.com>
2025-10-19 22:34:50 +0300

    Login Page completed

--- bloomlish/src/App.jsx
+++ bloomlish/src/App.jsx
@@ -1,8 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import BloomlishStartScreen from "./pages/BloomlishStartScreen";
-
+import LoginPage from "./pages/LoginPage";
 
 function App() {
-  return <BloomlishStartScreen />;
+  return (
+    <Router>
+      <Routes>
+        {/* Ana sayfa */}
+        <Route path="/" element={<BloomlishStartScreen />} />
+
+
+        {/* Giriş sayfası */}
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
